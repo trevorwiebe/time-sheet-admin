bb7f20d39965a18373532cf076a5884f1a442f28
trevor <thisistrevor4@gmail.com>
2025-01-20 10:11:34 -0600

    update agent

--- agent/agent/main.py
+++ agent/agent/main.py
@@ -7,7 +7,7 @@ from rich.text import Text
 
 from atomic_agents.agents.base_agent import BaseAgent, BaseAgentConfig, BaseAgentInputSchema, BaseAgentOutputSchema
 from atomic_agents.lib.components.system_prompt_generator import SystemPromptGenerator
-from agent.agent.code_base_provider import CodeBaseProcessor
+from tools.code_base_provider import CodeBaseProvider
 
 console = Console()
 
@@ -17,7 +17,14 @@ client = instructor.from_openai(openai.OpenAI(api_key=API_KEY))
 agent = BaseAgent(
     config=BaseAgentConfig(
         client=client,
-        model="gpt-4o-mini",
+        model="gpt-4o-mini"
+    )
+)
+
+searchResultAgent = BaseAgent(
+    config=BaseAgentConfig(
+        client=client,
+        model="gpt-4o-mini"
     )
 )
 
@@ -26,15 +33,14 @@ agent.memory.add_message("assistant", content=BaseAgentOutputSchema(chat_message
 
 console.print(Text(f"Assistant:\n{initial_message}", style="bold green"))
 
-while True:
-    user_input = console.input("You: ")
-
+def getCodeBase():
     folder_path = '/Users/trevorwiebe/Documents/WebApps/timesheet/build'
-    processor = CodeBaseProcessor(folder_path)
+    processor = CodeBaseProvider(folder_path)
     processor.run()
     code_base = processor.output
 
-    system_prompt_generator_custom = SystemPromptGenerator(
+def mainPrompt(code_base):
+    return SystemPromptGenerator(
         background=[
             "You are a helpful programming assistant that in knowledgable in html, css and javascript. Please review the codebase to determine best answer.",
             code_base
@@ -46,6 +52,14 @@ while True:
         ],
         output_instructions=["Your output should human readable with no added formatting characters."]
     )
+
+while True:
+    user_input = console.input("You: ")
+
+    # get existing code base into a variable
+    code_base = getCodeBase()
+
+    system_prompt_generator_custom = mainPrompt(code_base=code_base)
 
     agent.system_prompt_generator = system_prompt_generator_custom
 
