ff67e04e09d45c72211cac5304759392ed462e69
trevor <thisistrevor4@gmail.com>
2025-04-28 17:23:54 -0500

    Update date formatting in time-off utilities. Added 'timeZone: UTC' option to the date formatting options in both formatDateString and formatDateToLongString functions to ensure consistent date representation.

--- build/js/utils/time-off-utils.js
+++ build/js/utils/time-off-utils.js
@@ -1,10 +1,10 @@
 export function formatDateString(dateString) {
   const date = new Date(dateString);
-  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
   return date.toLocaleDateString('en-US', options);
 }
 
 export function formatDateToLongString(date) {
-  const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric' };
+  const options = { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', timeZone: 'UTC' };
   return date.toLocaleDateString('en-US', options);
 }
